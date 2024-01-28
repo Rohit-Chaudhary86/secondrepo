e78df8cfc92236d71f360659beebb7f7277ecf20
Rohit-Chaudhary86 <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2024-01-28 16:48:20 +0530

    Add files via upload

---
+++ q4.js
@@ -0,0 +1,7 @@
+
+
+//Operators in JavaScript perform operations on variables and values.
+//They enable mathematical calculations, comparison, logical operations, and more.
+//Essential for manipulating data, making decisions, and controlling program flow, operators enhance 
+//the flexibility and functionality of JavaScript, making it a powerful programming language for
+// web development and beyond.
